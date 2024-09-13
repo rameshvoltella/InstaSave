49d2dd7a468ac02c6d06fe6b10f02282dbf8fd6b
Ramesh M Nair <ramesh.nair@oliveboard.in>
2024-09-13 10:40:15 +0530

    password protection added

--- app/src/main/java/com/open/instafun/ui/components/VerticalPagerSample2.kt
+++ app/src/main/java/com/open/instafun/ui/components/VerticalPagerSample2.kt
@@ -1,28 +1,38 @@
 package com.open.instafun.ui.components
 
-
 import android.app.Activity
 import android.view.WindowManager
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.open.instafun.database.InstagramRepository
-import com.open.instafun.database.InstagramDBDownloader
+import androidx.compose.material.Text
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.TextField
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.open.instafun.viewmodel.AppViewmodel
 
 @OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @Composable
 fun VerticalPagerSample2(viewModel: AppViewmodel = hiltViewModel()) {
+    var password by remember { mutableStateOf("") }
+    var isPasswordCorrect by remember { mutableStateOf(false) }
+
+    // Define your correct password
+    val correctPassword = "@@@"
     val pagerState = rememberPagerState(pageCount = { viewModel.downloads.size })
     val context = LocalContext.current
 
@@ -49,15 +59,42 @@ fun VerticalPagerSample2(viewModel: AppViewmodel = hiltViewModel()) {
             activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         }
     }
-    VerticalPager(
-        state = pagerState,
-        modifier = Modifier
-            .fillMaxSize()
-            .pullRefresh(refreshState)
-    ) { page ->
-        val isPlaying = page == viewModel.currentPageIndex
-        val download = viewModel.downloads.getOrNull(page)
-        CustomMediaPlayerWithImages(download?.downloadUrl ?: "",isPlaying)
+    if (!isPasswordCorrect) {
+        // Password input section
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            TextField(
+                value = password,
+                onValueChange = { newPassword ->
+                    password = newPassword
+                    isPasswordCorrect = newPassword == correctPassword
+                },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+                    .padding(16.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = if (isPasswordCorrect) "Password correct, loading content..." else "Enter password",
+            )
+        }
+    } else {
+        VerticalPager(
+            state = pagerState,
+            modifier = Modifier
+                .fillMaxSize()
+                .pullRefresh(refreshState)
+        ) { page ->
+            val isPlaying = page == viewModel.currentPageIndex
+            val download = viewModel.downloads.getOrNull(page)
+            CustomMediaPlayerWithImages(download?.downloadUrl ?: "", isPlaying)
+        }
     }
 }
 
